9fa23aeafd9a68625b1f817bf7aef94c04c84de2
RogerChern <chenyuntao08@gmail.com>
2016-07-29 10:44:04 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function change_case(char) {
+    if (char >= 'a' && char <= 'z') {
+        if ((char.charCodeAt(0) - 97) % 2 === 1) {
+            return char;
+        } else {
+            return char.toUpperCase();
+        }
+    } else {
+        return char;
+    }
+}
+
+function calculate_hash(master_key, site_name, password_length) {
+    let hashObj = new jsSHA("SHA-512", "TEXT");
+    hashObj.update(master_key+site_name);
+    let password = hashObj.getHash("HEX").slice(0, password_length);
+    let new_pass = "";
+    for (i = 0; i < password.length; ++i) {
+        new_pass += change_case(password[i]);
+    }
+    return new_pass;
+}
+
+window.onload = function() {
+    document.getElementById("hash").addEventListener("click", function () {
+    master = document.getElementById("master").value;
+    site = document.getElementById("site").value;
+    document.getElementById("hashcode").innerText = calculate_hash(master, site, 16);
+});
+};
