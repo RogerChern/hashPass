578774eb326222f3556c6f0ec22241d7a5592121
RogerChern <chenyuntao08@gmail.com>
2016-08-17 11:44:21 +0800

    add enter key listener

--- script.js
+++ script.js
@@ -21,12 +21,22 @@ function calculate_hash(master_key, site_name, password_length) {
     return new_pass;
 }
 
-window.onload = function() {
-    document.getElementById("hash").addEventListener("click", function () {
+function show_hashcode() {
         master = document.getElementById("master").value;
         localStorage.setItem("master", master);
         site = document.getElementById("site").value;
         document.getElementById("hashcode").innerText = calculate_hash(master, site, 16);
-    });
+}
+
+window.onload = function() {
+    document.getElementById("hash").addEventListener("click", show_hashcode);
     document.getElementById("master").value = localStorage.getItem("master");
+    var input_box = document.getElementById("site");
+    input_box.focus();
+    input_box.addEventListener("keypress", function(e){
+        var key = e.which || e.keyCode;
+        if (key == 13) {
+            show_hashcode();
+        }
+    });
 };
