45bb7693528caac86a11e1b145f0eed8c5f28b5f
RogerChern <chenyuntao08@gmail.com>
2016-08-23 09:51:32 +0800

    add anonymous

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function change_case(char) {
 
 function calculate_hash(master_key, site_name, password_length) {
     var hashObj = new jsSHA("SHA-512", "TEXT");
-    hashObj.update(master_key+site_name);
+    hashObj.update(master_key+site_name.toLowerCase());
     var password = hashObj.getHash("HEX").slice(0, password_length);
     var new_pass = "";
     for (i = 0; i < password.length; ++i) {
@@ -22,15 +22,26 @@ function calculate_hash(master_key, site_name, password_length) {
 }
 
 function show_hashcode() {
-        master = document.getElementById("master").value;
-        localStorage.setItem("master", master);
-        site = document.getElementById("site").value;
-        document.getElementById("hashcode").innerText = calculate_hash(master, site, 16);
+    master = document.getElementById("master").value;
+    localStorage.setItem("master", master);
+    site = document.getElementById("site").value;
+    document.getElementById("hashcode").innerText = calculate_hash(master, site, 16);
+}
+
+function toggle_master_key() {
+    var masterDOM = document.getElementById("master");
+    var checked = document.getElementById("anonymous").checked;
+    if (checked) {
+        masterDOM.hidden = true;
+    } else {
+        masterDOM.hidden = false;
+    }
 }
 
 window.onload = function() {
     document.getElementById("hash").addEventListener("click", show_hashcode);
     document.getElementById("master").value = localStorage.getItem("master");
+    document.getElementById("anonymous").addEventListener("change", toggle_master_key);
     var input_box = document.getElementById("site");
     input_box.focus();
     input_box.addEventListener("keypress", function(e){
@@ -39,4 +50,5 @@ window.onload = function() {
             show_hashcode();
         }
     });
+    toggle_master_key();
 };
