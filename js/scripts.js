f774dcb90a4ffe70da72fb35188fd64248fcf9ae
RogerChern <chenyuntao08@gmail.com>
2017-02-11 17:31:26 +0800

    use bmd package

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 require('./sha512.js');
 require('./worker.js');
+require('bootstrap-material-design');
+require('bootstrap-material-design/dist/css/bootstrap-material-design.min.css');
+require('bootstrap-material-design/dist/css/ripples.min.css');
